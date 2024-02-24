deff5e5e27517c3891f5d3e79b9741e505417da1
Daniel Rrapi <uzo1209@gmail.com>
2024-02-24 11:16:41 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './views/navbar/navbar.component';
+import { FooterComponent } from './views/footer/footer.component';
+import { LandingPageComponent } from './views/landing-page/landing-page.component';
+import { HeroComponent } from './components/hero/hero.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    LandingPageComponent,
+    HeroComponent
   ],
   imports: [
     BrowserModule,
