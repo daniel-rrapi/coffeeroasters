42ae9861a1687fb246ac1ea0c38b25ddf3df6994
Daniel Rrapi <uzo1209@gmail.com>
2024-02-29 21:52:37 +0100

    Added "about us" page & adding "create your plane" page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from './views/landing-page/landing-page.component';
+import { AboutComponent } from './views/about/about.component';
+import { CreatePlanComponent } from './views/create-plan/create-plan.component';
 
 const routes: Routes = [
   {
     path: '',
     component: LandingPageComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+  {
+    path: 'create-your-plan',
+    component: CreatePlanComponent,
   },
 ];
 
---
+++ src/app/components/create-choose-your-coffee/create-choose-your-coffee.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-choose-your-coffee',
+  templateUrl: './create-choose-your-coffee.component.html',
+  styleUrls: ['./create-choose-your-coffee.component.scss']
+})
+export class CreateChooseYourCoffeeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
