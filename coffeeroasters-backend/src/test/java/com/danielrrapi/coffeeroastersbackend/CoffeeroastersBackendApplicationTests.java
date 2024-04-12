6c5c6b3e2cd9b8b3af72a4f4cfd9e9ed4a2017de
Daniel Rrapi <danielrrapi@proton.me>
2024-04-12 18:32:07 +0200

    Adjusting file

---
+++ coffeeroasters-backend/src/test/java/com/danielrrapi/coffeeroastersbackend/CoffeeroastersBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielrrapi.coffeeroastersbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeroastersBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
