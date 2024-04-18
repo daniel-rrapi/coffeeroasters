a18e354ee08e916731010268b70ea8a49dbd98ff
Daniel Rrapi <danielrrapi@proton.me>
2024-04-18 20:03:51 +0200

    Fixed navbar css

--- coffeeroasters-frontend/src/app/views/navbar/navbar.component.ts
+++ coffeeroasters-frontend/src/app/views/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 import { AuthService } from 'src/app/auth/auth.service';
 import { UserData } from 'src/app/interfaces/UserData';
 
@@ -9,12 +10,23 @@ import { UserData } from 'src/app/interfaces/UserData';
 })
 export class NavbarComponent implements OnInit {
   currentUser: null | UserData = null;
+  currentUrl = '';
 
-  constructor(private authSrv: AuthService) {
+  constructor(private authSrv: AuthService, private router: Router) {
     authSrv.user$.subscribe((res) => (this.currentUser = res));
+    router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.checkCurrentUrl();
+      }
+    });
+    this.checkCurrentUrl();
   }
 
   ngOnInit(): void {}
+
+  checkCurrentUrl() {
+    this.currentUrl = this.router.url;
+  }
 
   onClickLogout() {
     this.authSrv.logout();
